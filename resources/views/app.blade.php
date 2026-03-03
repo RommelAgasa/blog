8708e0d080b2e9ef8d274766aad227d30162dda7
_rommel <rommelagasa102@gmail.com>
2026-03-03 20:45:00 +0800

    Update font family to Lexend Deca and enhance HTML structure with preconnect links

--- resources/views/app.blade.php
+++ resources/views/app.blade.php
@@ -1,9 +1,12 @@
 <!DOCTYPE html>
-<html>
+<html lang="en">
   <head>
     <meta charset="utf-8" />
     <meta name="viewport" content="width=device-width, initial-scale=1.0, maximum-scale=1.0" />
     <meta name="csrf-token" content="{{ csrf_token() }}">
+    <link rel="preconnect" href="https://fonts.googleapis.com">
+    <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
+    <link href="https://fonts.googleapis.com/css2?family=Lexend+Deca:wght@100..900&display=swap" rel="stylesheet">
     @vite(['resources/css/app.css', 'resources/js/app.js'])
     @inertiaHead
   </head>
